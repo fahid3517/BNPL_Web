3550fa9a49baf37d7ac3ed630052ebde98c13e97
Admin <Admin@DESKTOP-D1O0BHE>
2022-01-05 17:05:11 +0500

    q

---
+++ BNPL_Web/Areas/SelfPortal/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using BNPL_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BNPL_Web.Areas.SelfPortal.Controllers
+{[Area("SelfPortal")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+       {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+                return RedirectToAction("Login","Account",new { area="SelfPortal"});
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

20d4c1cd31bd52e982307a30cfd244f2c03f7228
fahaddtech <85881595+fahaddtech@users.noreply.github.com>
2022-01-05 20:07:23 +0500

    User

---
+++ BNPL_Web.DataAccessLayer/IServices/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using BNPL_Web.Common.ViewModels;
+using BNPL_Web.Common.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNPL_Web.DataAccessLayer.IServices
+{
+    public interface IUserService
+    {
+        ResponseViewModel Add(UserViewModel value);
+    }
+}
---
+++ BNPL_Web/Controllers/ApiControllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using BNPL_Web.Common.ViewModels;
+using BNPL_Web.Common.ViewModels.Authorization;
+using BNPL_Web.DataAccessLayer.Helpers;
+using BNPL_Web.DataAccessLayer.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.DataAccessLayer.Utilities;
+using System.Net;
+
+namespace BNPL_Web.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService service;
+        public UserController(IServiceProvider provider)
+        {
+            service = (IUserService)provider.GetService(typeof(IUserService));
+
+        }
+
+
+        [HttpPost]
+        [Route("Post")]
+        public async Task<IActionResult> Post(UserViewModel model)
+        {
+           // var validate = CommonUtility.ValidateEmployee(model);
+            //if (!validate.success)
+            //{
+            //    return StatusCode((int)HttpStatusCode.BadRequest, validate.message);
+            //}
+
+            FunctionResult result = await IdentityHelper.createUser(model);
+            if (!result.success)
+            {
+                return StatusCode((int)HttpStatusCode.BadRequest, result.message);
+            }
+            //model.UserName = result.message;
+            //model.ChangedOn = DateTime.Now;
+            //model.ChangedBy = User.Identity.Name;
+            //var response = _EmployeeService.Add(model);
+            //return StatusCode((int)response.Status, response.obj);
+            return Ok();
+        }
+    }
+}

296928a94144989e11d414218fb14cad4b51dba5
fahaddtech <85881595+fahaddtech@users.noreply.github.com>
2022-01-05 16:32:33 +0500

    Login ViewModel

---
+++ BNPL_Web.Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BNPL_Web.Common.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}

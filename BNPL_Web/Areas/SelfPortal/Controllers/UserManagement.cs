8d3bb3364db8c14e4eb35702199ce735dc9e9c6a
Admin <Admin@DESKTOP-D1O0BHE>
2022-01-05 17:57:38 +0500

    Changes

---
+++ BNPL_Web/Areas/SelfPortal/Controllers/UserManagement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BNPL_Web.Areas.SelfPortal.Controllers
+{
+    [Area("SelfPortal")]
+    public class UserManagement : Controller
+    {
+        public IActionResult CustomerProfile()
+        {
+            return View();
+        }
+    }
+}

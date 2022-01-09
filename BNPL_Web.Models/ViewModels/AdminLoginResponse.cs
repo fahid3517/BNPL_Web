ff452a90b9c344f2ecb2e4087501720be2bbff75
fahaddtech <85881595+fahaddtech@users.noreply.github.com>
2022-01-09 12:06:43 +0500

    UserLoginapi and Token

---
+++ BNPL_Web.Models/ViewModels/AdminLoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BNPL_Web.Common.ViewModels
+{
+    public class AdminLoginResponse
+    {
+        [JsonProperty(PropertyName = "accessToken")]
+        public string AccessToken { get; set; } = "";
+    }
+}

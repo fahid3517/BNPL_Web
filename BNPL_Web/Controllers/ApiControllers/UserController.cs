26a325ee2b5b645a4d52abfce8097a55ba52ba0f
fahaddtech <85881595+fahaddtech@users.noreply.github.com>
2022-01-12 20:42:41 +0500

    sms

--- BNPL_Web/Controllers/ApiControllers/UserController.cs
+++ BNPL_Web/Controllers/ApiControllers/UserController.cs
@@ -100,22 +100,22 @@ namespace BNPL_Web.Controllers.ApiControllers
             var text = "Hye Verification Code is" + OtpCode;
 
             var commandText = "INSERT INTO InsertSms (Body, ToAddress, FromAddress, ChannelID,StatusID, DataCoding, CustomField1, CustomField2) VALUES (@body, @to, @FromAddress, @ChannelID,'SCHEDULED',0,@CustomField1, @CustomField2);";
-            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("OTPConnection:DefaultConnection")))
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("SMSConnection")))
             {
                 SqlCommand command = new SqlCommand(commandText, connection);
                 command.Parameters.AddWithValue("@to", To);
 
                 command.Parameters.AddWithValue("@body", text);
 
-                command.Parameters.AddWithValue("@FromAddress", System.Configuration.ConfigurationManager.AppSettings[""]);
+                command.Parameters.AddWithValue("@FromAddress", this._configuration.GetValue<String>("AppSettings:SMS_DevSmsService_FromAddress"));
 
 
 
-                command.Parameters.AddWithValue("@ChannelId", System.Configuration.ConfigurationManager.AppSettings[""]);
-                var configvalue1 = this._configuration.GetValue<String>("SMS_DevSmsService_CustomField2_en");
-                command.Parameters.AddWithValue("@CustomField1", this._configuration.GetValue<String>("SMS_DevSmsService_CustomField2_en"));
+                command.Parameters.AddWithValue("@ChannelId", this._configuration.GetValue<String>("AppSettings:SMS_DevSmsService_ChannelId"));
+                var configvalue1 = this._configuration.GetValue<String>("AppSettings:SMS_DevSmsService_CustomField2_en");
+                command.Parameters.AddWithValue("@CustomField1", this._configuration.GetValue<String>("AppSettings:SMS_DevSmsService_CustomField1"));
 
-                command.Parameters.AddWithValue("@CustomField2", this._configuration.GetValue<String>("SMS_DevSmsService_CustomField2_en"));
+                command.Parameters.AddWithValue("@CustomField2", this._configuration.GetValue<String>("AppSettings:'SMS_DevSmsService_CustomField2_en'"));
                 connection.Open();
                 command.ExecuteNonQuery();
                 return true;
